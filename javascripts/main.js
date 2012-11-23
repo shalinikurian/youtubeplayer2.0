ab7d10b8da511be9e6c6e3a29bfb4060b957c7f6
Shalini <shalinikurian@gmail.com>
2012-11-23 00:05:33 -0800

    add playlists , delete playlists

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,6 +5,16 @@ var vent = _.extend({} , Backbone.Events);
  */
 
 var Song = Backbone.Model.extend({});
+
+var Playlist = Backbone.Model.extend({
+	defaults: {
+		name: "New Playlist"
+	},
+	
+	initialize: function() {
+		
+	}
+});
 /*
  * Views
  */
@@ -93,6 +103,124 @@ var SearchResultSongView = Backbone.View.extend({
 	render: function() {
 		this.$searchResultItem.html(this.template(this.model.toJSON()));
 		return this;
+	},
+	
+	events: {
+		'click .thumbnail' : 'playSong'
+	},
+	
+	playSong: function() { //TODO
+		
+	}
+});
+
+var PlayListsView = Backbone.View.extend({
+	el : '#left_nav',
+	
+	initialize: function(args){
+		this.vent = args.vent;
+		this.$textBox = $('#new_playlist_name');
+		this.$playlists = $('#playlists');
+		$(this.el).menu();
+		this.render();
+	},
+	
+	events: {
+		'click #add_playlist' : 'showNewPlayListTextBox',
+		'keyup #new_playlist_name' : 'addNewPlayList'
+	},
+	
+	render: function() {
+		_.each(playlistsCollection.models, function(playlist){
+			this.appendPlaylistToView(playlist);
+		}.bind(this));
+	},
+	
+	showNewPlayListTextBox: function(e) {
+		this.$textBox.val('');
+		this.$textBox.slideDown('fast', function(){});
+		this.$textBox.focus();
+	},
+	
+	addNewPlayList: function(e) {
+		if (e.which == 27) {
+			this.$textBox.slideUp('fast', function(){});
+		}
+		
+		if (e.keyCode == 13) {
+			this.$textBox.slideUp('fast', function(){});
+			var playlistName = this.$textBox.val();
+			var order = playlistsCollection.getNextOrder();
+			playlistsCollection.create({
+				name: playlistName,
+				order: order
+			});
+			playlistsCollection.last().save();
+			this.appendPlaylistToView(playlistsCollection.last());
+		}
+	},
+		
+	appendPlaylistToView: function(playlist) {
+		var playlistView = new PlayListView({
+			model: playlist,
+			vent: vent
+		});
+			
+		this.$playlists.append(playlistView.render().el);
+	}
+});
+
+var PlayListView = Backbone.View.extend({
+	tagName : 'div',
+	
+	initialize: function(args) {
+		this.vent = args.vent;
+		this.model.bind('destroy', this.remove, this);
+		this.$template = _.template($('#playlist_template').html());
+	},
+	
+	events: {
+		'click .delete' : 'deletePlaylist'
+	},
+	
+	deletePlaylist: function(e){
+		this.model.destroy();
+	},
+	
+	remove: function() {
+		$(this.el).remove();
+	},
+	
+	showOptions: function(e) {
+		var $target = $(e.target);
+		$(this.el).find('.options').show();
+	},
+	
+	hideOptions: function(e) {
+		var $target = $(e.target);
+		$(this.el).find('.options').hide();
+	},
+	
+	render: function() {
+		$(this.el).html(this.$template(this.model.toJSON()));
+		return this;
+	},
+
+});
+
+
+/*
+ * collections
+ */
+
+var PlayListCollection = Backbone.Collection.extend({
+	model: Playlist,
+	
+	localStorage: new Store("playlists"),
+	
+	getNextOrder : function() {
+		if (this.length == 0) return 1;
+		return this.last().get('order') + 1;
 	}
 });
 /*
@@ -100,6 +228,9 @@ var SearchResultSongView = Backbone.View.extend({
  */
 function appInit() {
 	window.searchResultsView = new SearchResultsView({el:'body', vent:vent, maxResults:10});
+	window.playlistsCollection = new PlayListCollection;
+	playlistsCollection.fetch();
+    window.playListsView = new PlayListsView({vent: vent});
 }
 
 $(document).ready(function() {
